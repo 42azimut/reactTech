265561b0fa6d47ce00dc1643d1267f77306f2fe5
42azimut <azimutuniverse@gmail.com>
2022-01-12 08:59:41 +0900

    복습 8장 Info.js setVisible() 만들기

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('유즈 이펙트 랜더링 완료');
+    console.log(name, nickname);
+    return () => {
+      console.log('클린업');
+      console.log(name);
+    };
+  }, [name]);
 
   const 함수명1 = (e) => {
     setName(e.target.value);
