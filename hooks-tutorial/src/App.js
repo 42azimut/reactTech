451d6b302e123afb7fc7796d0256e043b172ab90
42azimut <azimutuniverse@gmail.com>
2022-01-12 12:34:14 +0900

    복습 8장 useReducer  dispatch 헷갈림

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import UseReducerCounter from './UseReducerCounter';
+import UseReducerInfo from './UseReducerInfo';
 
 const App = () => {
-  return (
-    <div>
-      <UseReducerCounter />
-    </div>
-  );
+  return <UseReducerInfo />;
 };
 
 export default App;
---
+++ hooks-tutorial/src/UseReducerInfo.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+
+const UseReducerInfo = () => {
+  const [state, dispatch] = useReducer(reducer, { name: '', nickname: '' });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    console.log(dispatch(e.target));
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>이름: {name}</div>
+        <div>닉네임: {nickname}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducerInfo;
