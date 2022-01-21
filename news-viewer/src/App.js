939a30d9c47a9ab2fd38b0eec2d9f931f5253087
42azimut <azimutuniverse@gmail.com>
2022-01-21 09:45:13 +0900

    뉴스 뷰어 기본 세팅하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,35 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 const App = () => {
-  const [data1, setData] = useState(null);
-  const URL = 'https://jsonplaceholder.typicode.com/todos/1';
-  const onClick = async () => {
-    try {
-      const result = await axios.get(URL);
-      //console.log(result);
-      setData(result.data); //state 의 데이터가 아님! res.data 객체임!
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data1 && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data1.title, null, 2)}
-          readOnly
-        />
-      )}
-      {data1 && (
-        <textarea rows={7} value={JSON.stringify(data1, null, 2)} readOnly />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
-
 export default App;
