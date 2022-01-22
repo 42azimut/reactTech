dee1baf5f87358527991936f009583715bb30b1e
42azimut <azimutuniverse@gmail.com>
2022-01-22 21:39:57 +0900

    뉴스 뷰어 끝! react-router-dom 5.x 버전 이상만 가능(책에서는)

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,15 +1,13 @@
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
-import { useCallback, useState } from 'react';
-const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
+//import NewsList from './components/NewsList';
+//import Categories from './components/Categories';
+//import { useCallback, useState } from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />;
-    </>
-  );
+const App = () => {
+  //const [category, setCategory] = useState('all');
+  //const onSelect = useCallback((category) => setCategory(category), []);
+
+  return <Route path="/:category?" component={NewsPage} />;
 };
 export default App;
