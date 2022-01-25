c2e115b13121a23cdb4eefbfecc85a7ece5b0758
42azimut <azimutuniverse@gmail.com>
2022-01-25 21:36:52 +0900

    nomad 04 props memo(Btn) ==> MemorizedBtn />

--- nomad/04_props/props-study/src/App.js
+++ nomad/04_props/props-study/src/App.js
@@ -1,13 +1,18 @@
+import { useState, memo } from 'react';
 import './App.css';
 import Btn from './Btn';
 
+const MemorizedBtn = memo(Btn);
+
 function App() {
+  const [value, setValue] = useState('Save Changes');
+  const changeValue = () => setValue('Revert Changes');
   return (
     <div className="App">
       <h1>Props Tutorial</h1>
 
-      <Btn text="Save Changes" big={true} />
-      <Btn text="Continue" big={false} />
+      <MemorizedBtn text={value} changeValue={changeValue} />
+      <MemorizedBtn text="Continue" />
     </div>
   );
 }
--- nomad/04_props/props-study/src/Btn.js
+++ nomad/04_props/props-study/src/Btn.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Btn = ({ text, big }) => {
-  console.log(text, big);
+const Btn = ({ text, changeValue }) => {
+  console.log(text, 'was render');
   return (
     <button
+      onClick={changeValue}
       style={{
         backgroundColor: 'tomato',
-        fontSize: big ? 20 : 16,
+        fontSize: 20,
         color: 'white',
         padding: '10px 20px',
         border: 0,
@@ -17,5 +18,5 @@ const Btn = ({ text, big }) => {
     </button>
   );
 };
-
+//const MemorizedBtn = memo(Btn); App 에서 작성해야함!
 export default Btn;
